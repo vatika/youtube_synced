7fdd2c0dd07777bca7769e455a0a9fec19de72e5
Sandeep <sandeep@Sandeeps-MacBook-Pro.local>
2018-08-28 13:11:36 +0530

    added basic chat

---
+++ index.js
@@ -0,0 +1,22 @@
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+http.listen('2000', function() {
+	console.log('listening at port 2000');
+});
+
+app.get('/', function(req, res) {
+	res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', function(socket) {
+	console.log('a user has connected');
+	socket.on('disconnect', function() {
+		console.log('a user has disconnected');
+	})
+  	socket.on('chat message', function(msg){
+    		io.emit('chat message', msg);
+	});
+});
+
